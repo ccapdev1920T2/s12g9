d57f7a5ae9306c6dcfbf03ad822a5bcbac9fcae8
Ivy Tan <tanivy31@gmail.com>
2020-04-15 09:39:47 +0800

    close

--- models/db.js
+++ models/db.js
@@ -9,7 +9,7 @@ const client = mongodb.MongoClient;
 const url = `mongodb://user:pass123@database-shard-00-00-ourwj.mongodb.net:27017,database-shard-00-01-ourwj.mongodb.net:27017,database-shard-00-02-ourwj.mongodb.net:27017/test?ssl=true&replicaSet=Database-shard-0&authSource=admin&retryWrites=true&w=majority`;
 const options = { useUnifiedTopology: true, useNewUrlParser: true};
 
-const dbName = 'HotelParaiso';
+const dbName = 'test';
 
 const database = {
 
@@ -18,7 +18,7 @@ const database = {
         client.connect(url, options, function(err, db) {
             if (err) throw err;
             // console.log('Database created!');
-            db.close();
+            // db.close();
         });
     },
 
@@ -29,7 +29,7 @@ const database = {
             database.createCollection(collection, function(err, res) {
                 if (err) throw err;
                 // console.log('Collection ' + collection + ' created');
-                db.close();
+                // db.close();
             });
         });
     },
@@ -41,7 +41,7 @@ const database = {
             database.collection(collection).insertOne(doc, function(err, res) {
                 if (err) throw err;
                 // console.log('1 document inserted');
-                db.close();
+                // db.close();
                 return callback(res);
             });
         });
@@ -54,7 +54,7 @@ const database = {
             database.collection(collection).insertMany(docs, function(err, res) {
                 if (err) throw err;
                 // console.log('Documents inserted: ' + res.insertedCount);
-                db.close();
+                // db.close();
                 return callback(res);
             });
         });
@@ -67,7 +67,7 @@ const database = {
             var database = db.db(dbName);
             database.collection(collection).findOne(query, function(err, result) {
                 if (err) throw err;
-                db.close();
+                // db.close();
                 return callback(result);
             });
         });
@@ -80,7 +80,7 @@ const database = {
             database.collection(collection).find(query, { projection: projection })
                 .sort(sort).toArray(function(err, result) {
                     if (err) throw err;
-                    db.close();
+                    // db.close();
                     return callback(result);
                 });
         });
@@ -95,7 +95,7 @@ const database = {
             database.collection(collection).deleteOne(filter, function(err, res) {
                 if (err) throw err;
                 // console.log('1 document deleted');
-                db.close();
+                // db.close();
                 return callback(res);
             });
         });
@@ -110,7 +110,7 @@ const database = {
             database.collection(collection).deleteMany(filter, function(err, res) {
                 if (err) throw err;
                 // console.log('Documents deleted: ' + res.deletedCount);
-                db.close();
+                // db.close();
                 return callback(res);
             });
         });
@@ -124,7 +124,7 @@ const database = {
             database.collection(collection).drop(function(err, res) {
                 if (err) throw err;
                 // console.log('Collection ' + collection + ' deleted');
-                db.close();
+                // db.close();
             });
         });
     },
@@ -137,7 +137,7 @@ const database = {
             database.collection(collection).updateOne(filter, update, function(err, res) {
                 if (err) throw err;
                 // console.log('1 document updated');
-                db.close();
+                // db.close();
                 return callback(res);
             });
         });
@@ -151,7 +151,7 @@ const database = {
             database.collection(collection).updateMany(filter, update, function(err, res) {
                 if (err) throw err;
                 // console.log('Documents updated: ' + res.modifiedCount);
-                db.close();
+                // db.close();
                 return callback(res);
             });
         });
